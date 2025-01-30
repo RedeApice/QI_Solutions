c1c0a9442f865edd43ce2c19f302a2cf9c467f1b
RedeApice <infra@redeapice.com.br>
2025-01-30 16:29:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+from sqlalchemy import create_engine
 
 load_dotenv()
 
@@ -29,5 +30,19 @@ df_pago = df[df['valor_pago'].isnull() | (df['valor_pago'] == 0)]
 # print(df_2025)
 print(df_pago)
 
-df.to_csv('df_pago.csv', index=False)
+# df.to_csv('df_pago.csv', index=False)
 # df.to_csv('df_2025.csv', index=False)
+
+user = os.getenv('usuario')
+password = os.getenv('senha')
+hostname = os.getenv('host')
+dbname = os.getenv('nome_banco')
+table = os.getenv('tbl_name')
+
+engine = create_engine(f'mysql+pymysql://{user}:{password}@{hostname}/{dbname}')
+
+df = pd.read_csv('df_pago.csv')
+
+df.to_sql(table, con=engine, if_exists='append', index=False)
+
+print('Dados inseridos.')
